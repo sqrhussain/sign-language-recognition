fa4baf91e5facfc44126206eab9d618acf249c0f
sqrhussain <sqr.hussain@hotmail.com>
2016-03-01 03:18:38 +0200

    Update SignRecognizer.m

--- SignRecognizer.m
+++ SignRecognizer.m
@@ -2,27 +2,28 @@ function  hsv = SignRecognizer()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-    addpath('./circular cut/');
-    
-    % options.FramesAcquired = 10;
-    % options.FrameGrabInterval = 40;
-    % 
-    % [h , vid] = initVideo( options );
-    % start(vid);
-    hsv = [];
-    for i = 1:10
-        snapshot = imread('http://192.168.1.7:8080/shot.jpg');
-        hsv =  rgb2hsv(snapshot) ;
-         tic
-        info = processImage(hsv(:,:,3));
-        toc
+options.FramesAcquired = 10;
+options.FrameGrabInterval = 40;
 
-        plotResults(h , snapshot ,info);
-        pause(1);
-    end
+[h , vid] = initVideo( options );
+start(vid);
+hsv = [];
+while(vid.FramesAcquired <= options.FramesAcquired )
+    
+    snapshot =  getdata(vid,1) ;
+    flushdata(vid);
+
+    hsv =  rgb2hsv(snapshot) ;
+       
+    tic
+    info = processImage( hsv(:,:,3) );
+    toc
+    
+    plotResults(h , snapshot ,info);
+
+end
+stop(vid);
+%stop(imaqfind)
 end
 
 
-
-
-[h , vid] = initVideo( options );
