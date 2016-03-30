01779dde813a4e5b0a980821bc3ac0998cd5bc44
sqrhussain <sqr.hussain@hotmail.com>
2016-03-30 23:59:33 +0300

    Update extract_skin.m

--- face_dependent/extract_skin.m
+++ face_dependent/extract_skin.m
@@ -9,7 +9,7 @@ function bw = extract_skin(rgb, face_rgb)
     meancb = mean2(face_cb);
     meancr = mean2(face_cr);
 % standard deviation as a threshold
-    stdcbcr = mean2(sqrt((face_cb-meancb).^2+(face_cr-meancr).^2));
+   % stdcbcr = mean2(sqrt((face_cb-meancb).^2+(face_cr-meancr).^2));
 
 % get Cb, Cr of the image
     ycbcr = im2double(rgb2ycbcr(rgb));
@@ -18,7 +18,11 @@ function bw = extract_skin(rgb, face_rgb)
 
 % distance from face mean
     skin_mean_distance = sqrt((cb-meancb).^2+(cr-meancr).^2);
-    bw = ~im2bw(skin_mean_distance,stdcbcr);
-
-    bw = remove_noise(bw);
+    skin_dist_gray = mat2gray(skin_mean_distance);
+    bw = ~im2bw(skin_dist_gray,graythresh(skin_dist_gray));
+%     bw = remove_noise(bw);
+%      subplot(1,2,1); subimage(rgb);title('RGB');
+%      subplot(1,2,2); subimage(bw);title('Skin');
+    %bw = ~im2bw(skin_mean_distance,stdcbcr*2);
+    
  end
